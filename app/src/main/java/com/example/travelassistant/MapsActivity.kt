cf976609934bf35b65f48c16388d3df3a014b82a
Jokasxy <josip_1997@live.com>
2020-07-06 19:40:43 +0200

    Current location listener added

--- app/src/main/java/com/example/travelassistant/MapsActivity.kt
+++ app/src/main/java/com/example/travelassistant/MapsActivity.kt
@@ -1,7 +1,18 @@
 package com.example.travelassistant
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.rma5_maps.hasPermissionCompat
+import com.example.rma5_maps.requestPermissionCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,6 +25,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
 {
 
     private lateinit var map: GoogleMap
+    private val locationPermission = Manifest.permission.ACCESS_FINE_LOCATION
+    private val locationRequestCode = 10
+    private lateinit var locationManager: LocationManager
+
+    private val locationListener = object: LocationListener
+    {
+        override fun onProviderEnabled(provider: String?) { }
+        override fun onProviderDisabled(provider: String?) { }
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) { }
+        override fun onLocationChanged(location: Location?)
+        {
+            updateLocationDisplay(location)
+        }
+    }
+
+    private fun updateLocationDisplay(location: Location?)
+    {
+        if (location != null)
+        {
+            val modifiedLocation = LatLng(location.latitude, location.longitude)
+            map.addMarker(MarkerOptions().position(modifiedLocation).title("Current location"))
+            map.moveCamera(CameraUpdateFactory.newLatLng(modifiedLocation))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -23,24 +58,63 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap)
     {
         map = googleMap
+        trackLocation()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun trackLocation()
+    {
+        if(hasPermissionCompat(locationPermission))
+        {
+            startTrackingLocation()
+        }
+        else
+        {
+            requestPermissionCompat(arrayOf(locationPermission), locationRequestCode)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray)
+    {
+        when(requestCode)
+        {
+            locationRequestCode ->
+            {
+                if(grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    trackLocation()
+                else
+                    Toast.makeText(this, R.string.permissionNotGranted, Toast.LENGTH_SHORT).show()
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+    private fun startTrackingLocation()
+    {
+        Log.d("TAG", "Tracking location")
+        val criteria: Criteria = Criteria()
+        criteria.accuracy = Criteria.ACCURACY_FINE
+        val provider = locationManager.getBestProvider(criteria, true)
+        val minTime = 1000L
+        val minDistance = 10.0F
+        try
+        {
+            locationManager.requestLocationUpdates(provider, minTime, minDistance, locationListener)
+        }
+        catch (e: SecurityException)
+        {
+            Toast.makeText(this, R.string.permissionNotGranted, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onPause()
+    {
+        super.onPause()
+        locationManager.removeUpdates(locationListener)
     }
 }
---
+++ app/src/main/java/com/example/travelassistant/PermissionUtils.kt
@@ -0,0 +1,20 @@
+package com.example.rma5_maps
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+fun AppCompatActivity.hasPermissionCompat (permission: String): Boolean
+{
+    return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+}
+
+fun AppCompatActivity.shouldShowRationaleCompat(permission: String): Boolean
+{
+    return ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
+}
+
+fun AppCompatActivity.requestPermissionCompat(permission: Array<String>, requestCode: Int)
+{
+    ActivityCompat.requestPermissions(this, permission, requestCode)
+}
